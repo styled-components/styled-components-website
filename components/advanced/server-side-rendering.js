bbdada9090056ab6692739b148ffed27ca656efa
Phil Plückthun <phil@plckthn.me>
2017-05-21 19:06:24 +0100

    Improve CodeBlock

--- components/advanced/server-side-rendering.js
+++ components/advanced/server-side-rendering.js
@@ -81,7 +81,7 @@ const ServerSideRendering = () => (
       The basic API goes as follows:
     </p>
 
-    <CodeBlock code={sample} />
+    <CodeBlock code={sample} language="jsx" />
 
     <p>
       The <Code>collectStyles</Code> method wraps your element in a provider. Optionally you can use
@@ -89,7 +89,7 @@ const ServerSideRendering = () => (
       use it on the client-side.
     </p>
 
-    <CodeBlock code={managerSample} />
+    <CodeBlock code={managerSample} language="jsx" />
 
     <p>
       The <Code>sheet.getStyleTags()</Code> returns a string of multiple <Code>&lt;style&gt;</Code> tags.
@@ -107,7 +107,7 @@ const ServerSideRendering = () => (
         the provided example in their repository for v1.
       </p>
 
-      <CodeBlock code={nextSample} />
+      <CodeBlock code={nextSample} language="jsx" />
 
       <p>
         Here we're wrapping the <Code>Main</Code> component, which contains the Next.js app, and are using this
