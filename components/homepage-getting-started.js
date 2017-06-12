5b98d4bd584e335389868742fd84a0e8349ebda0
meliani omar el farouk (younes) <supernova23@users.noreply.github.com>
2017-06-12 05:10:16 +0100

    fixing gzipped size image

--- components/homepage-getting-started.js
+++ components/homepage-getting-started.js
@@ -109,7 +109,7 @@ const HomepageGettingStarted = () => (
       	<Badge src="https://img.shields.io/npm/v/styled-components.svg" alt="Current version" />
       </a>
       <Badge src="https://img.shields.io/npm/dm/styled-components.svg?maxAge=3600" alt="Monthly downloads" />
-      <Badge src="http://img.badgesize.io/https://unpkg.com/styled-components/dist/styled-components.min.js?compression=gzip&label=gzip%20size" alt="Gzipped size" />
+      <Badge src="https://img.shields.io/badge/gzip size-13.3 kB-brightgreen.svg?maxAge=3600" alt="Gzipped size" />
       <a href="https://spectrum.chat/styled-components">
         <Badge alt="Join the community on Spectrum" src="https://withspectrum.github.io/badge/badge.svg" />
       </a>
