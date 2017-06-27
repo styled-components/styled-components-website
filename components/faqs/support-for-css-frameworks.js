8f509d7b5d0b2822d046cca52ad6d1f7e7de617e
Jikku Jose <jikkujose@gmail.com>
2017-06-27 22:59:46 +0530

    Add section about CSS framework in FAQ

---
+++ components/faqs/support-for-css-frameworks.js
@@ -0,0 +1,76 @@
+import React from 'react'
+
+import SectionLayout from '../SectionLayout'
+import CodeBlock from '../CodeBlock'
+
+const sampleEqualDivider = (`
+const EqualDivider = styled.div\`
+  display: flex;
+  margin: 0.5rem;
+  padding: 1rem;
+  background: papayawhip;
+  \${props => props.vertical && 'flex-direction: column;'}
+
+  > * {
+    flex: 1;
+
+    &:not(:first-child) {
+      \${props => props.vertical ? 'margin-top' : 'margin-left'}: 1rem;
+    }
+  }
+\`
+
+const Child = styled.div\`
+  padding: 0.25rem 0.5rem;
+  background: palevioletred;
+\`
+
+render(
+  <div>
+  <EqualDivider>
+    <Child>First</Child>
+    <Child>Second</Child>
+    <Child>Third</Child>
+  </EqualDivider>
+  <EqualDivider vertical>
+    <Child>First</Child>
+    <Child>Second</Child>
+    <Child>Third</Child>
+  </EqualDivider>
+  </div>
+);
+`).trim()
+
+const sampleTachyonsExample = (`
+const Button = styled.button.attrs({
+  className: 'pa4 ba b--black-80',
+})\`
+	background: red;
+	color: white;
+
+	font-size: 1em;
+	margin: 1em;
+	padding: 0.25em 1em;
+	border: 2px solid palevioletred;
+	border-radius: 3px;
+\`;
+
+/* And use in render as: */
+
+render(
+	<div>
+		<Button>Hello from Tachyons</Button>
+	</div>
+);
+`).trim()
+
+const CSSFrameworks = () => (
+  <SectionLayout title="Support for CSS Frameworks">
+    <p>Class names defined from a referenced CSS file can be used <b>along</b> with the style definitions! This will let styles to be separated from the markup making it very clean & readable.</p>
+    <p>Consider we are using the <b><a href="http://tachyons.io/">Tachyons</a></b> library. The classes <b>pa4</b>, <b>ba</b>, <b>bg--black-80</b> from the library can be provided along with the style definition so that the markup will be clean without any style related noise.</p>
+    <p>Same can be done with Bootstrap, Foundation or any similar CSS frameworks.</p>
+    <CodeBlock code={sampleTachyonsExample} language="jsx" />
+  </SectionLayout>
+)
+
+export default CSSFrameworks
--- pages/docs/faqs.js
+++ pages/docs/faqs.js
@@ -3,11 +3,13 @@ import DocsLayout from '../../components/DocsLayout'
 
 import Nesting from '../../components/faqs/nesting'
 import ReverseSelectors from '../../components/faqs/reverse-selectors'
+import CSSFrameworks from '../../components/faqs/support-for-css-frameworks'
 
 const FAQs = () => (
   <DocsLayout title="FAQs">
     <Nesting />
     <ReverseSelectors />
+    <CSSFrameworks />
   </DocsLayout>
 )
 
