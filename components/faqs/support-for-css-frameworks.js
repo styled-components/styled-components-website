75d375ed018422e03a5e55a5e712eba48522a0e4
Jikku Jose <jikkujose@gmail.com>
2017-06-27 23:02:24 +0530

    Remove unwanted chunks

--- components/faqs/support-for-css-frameworks.js
+++ components/faqs/support-for-css-frameworks.js
@@ -3,44 +3,6 @@ import React from 'react'
 import SectionLayout from '../SectionLayout'
 import CodeBlock from '../CodeBlock'
 
-const sampleEqualDivider = (`
-const EqualDivider = styled.div\`
-  display: flex;
-  margin: 0.5rem;
-  padding: 1rem;
-  background: papayawhip;
-  \${props => props.vertical && 'flex-direction: column;'}
-
-  > * {
-    flex: 1;
-
-    &:not(:first-child) {
-      \${props => props.vertical ? 'margin-top' : 'margin-left'}: 1rem;
-    }
-  }
-\`
-
-const Child = styled.div\`
-  padding: 0.25rem 0.5rem;
-  background: palevioletred;
-\`
-
-render(
-  <div>
-  <EqualDivider>
-    <Child>First</Child>
-    <Child>Second</Child>
-    <Child>Third</Child>
-  </EqualDivider>
-  <EqualDivider vertical>
-    <Child>First</Child>
-    <Child>Second</Child>
-    <Child>Third</Child>
-  </EqualDivider>
-  </div>
-);
-`).trim()
-
 const sampleTachyonsExample = (`
 const Button = styled.button.attrs({
   className: 'pa4 ba b--black-80',
