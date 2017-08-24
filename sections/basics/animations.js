42fafa55235fffb0d6b5ff6c6df327b7ffc24290
Phil Plückthun <phil@plckthn.me>
2017-08-24 04:31:00 +0100

    Convert missing animations sections to markdown

--- sections/basics/animations.js
+++ sections/basics/animations.js
@@ -1,51 +1,39 @@
-import React from 'react'
-import SectionLayout from '../SectionLayout'
-import LiveEdit from '../LiveEdit'
-import Code from 'components/Code'
+import md from 'components/md'
 
-const sample = (`
-// keyframes returns a unique name based on a hash of the contents of the keyframes
-const rotate360 = keyframes\`
-  from {
-    transform: rotate(0deg);
-  }
+const Animations = () => md`
+  ## Animations
 
-  to {
-    transform: rotate(360deg);
-  }
-\`;
+  CSS animations with \`@keyframes\` aren't scoped to a single component but you still don't want them
+  to be global. This is why we export a \`keyframes\` helper which will generate a unique name for your
+  keyframes. You can then use that unique name throughout your app.
 
-// Here we create a component that will rotate everything we pass in over two seconds
-const Rotate = styled.div\`
-  display: inline-block;
-  animation: \${rotate360} 2s linear infinite;
-  padding: 2rem 1rem;
-  font-size: 1.2rem;
-\`;
+  This way, you get all the benefits of using JavaScript, are avoiding name clashes and get your keyframes
+  like always:
 
-render(
-  <Rotate>&lt; 💅 &gt;</Rotate>
-)
-`).trim()
+  \`\`\`react
+  // keyframes returns a unique name based on a hash of the contents of the keyframes
+  const rotate360 = keyframes\`
+    from {
+      transform: rotate(0deg);
+    }
 
-const Animations = () => (
-  <SectionLayout title="Animations">
-    <p>
-      CSS animations with <Code>@keyframes</Code> aren't scoped to a single component but you still don't want them
-      to be global. This is why we export a <Code>keyframes</Code> helper which will generate a unique name for your
-      keyframes. You can then use that unique name throughout your app.
-    </p>
+    to {
+      transform: rotate(360deg);
+    }
+  \`;
 
-    <p>
-      This way, you get all the benefits of using JavaScript, are avoiding name clashes and get your keyframes
-      like always:
-    </p>
+  // Here we create a component that will rotate everything we pass in over two seconds
+  const Rotate = styled.div\`
+    display: inline-block;
+    animation: \${rotate360} 2s linear infinite;
+    padding: 2rem 1rem;
+    font-size: 1.2rem;
+  \`;
 
-    <LiveEdit
-      code={sample}
-      noInline
-    />
-  </SectionLayout>
-)
+  render(
+    <Rotate>&lt; 💅 &gt;</Rotate>
+  );
+  \`\`\`
+`
 
 export default Animations
