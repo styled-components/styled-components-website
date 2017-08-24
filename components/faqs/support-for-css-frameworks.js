25ad94457e339c19796109e946c31c1f990643c2
Phil Plückthun <phil@plckthn.me>
2017-08-24 02:32:57 +0100

    Convert all sections in faqs to markdown

--- components/faqs/support-for-css-frameworks.js
+++ components/faqs/support-for-css-frameworks.js
@@ -1,39 +1,37 @@
-import React from 'react'
+import md from '../md'
 
-import SectionLayout from '../SectionLayout'
-import Code from '../Code'
-import Link from '../Link'
-import LiveEdit from '../LiveEdit'
-import Link from '../Link'
+const CSSFrameworks = () => md`
+  ## Can I use CSS frameworks?
 
-const sampleTachyonsExample = (`
-const Button = styled.button.attrs({
-  className: 'small',
-})\`
-	background: black;
-  color: white;
-  cursor: pointer;
-	margin: 1em;
-	padding: 0.25em 1em;
-	border: 2px solid black;
-	border-radius: 3px;
-\`;
+  Integrating an existing CSS framework with styled-components is really easy!
+  You can use its existing class names alongside your components.
 
-render(
-	<div>
-<Button>Styled Components</Button>
-		<Button>The new way to style components!</Button>
-	</div>
-);
-`).trim()
+  Consider you have an existing app with some CSS that have the classes: \`.small\` and \`.big\`.
+  Try to swap out the \`.small\` class with \`.big\` in the example below:
 
-const CSSFrameworks = () => (
-  <SectionLayout title="Can I use CSS frameworks?">
-    <p>Integrating an existing CSS framework with styled-components is really easy! You can use its existing class names alongside your components.</p>
-    <p>Consider you have an existing app with some CSS that have the classes: <Code>.small</Code> and <Code>.big</Code>. Now try swapping the <Code>.small</Code> class with <Code>.big</Code>!</p>
-    <LiveEdit code={sampleTachyonsExample} noInline />
-    <p>Do read about <Link inline href="/docs/api#attrs">attrs</Link> to know how arbitary attributes can be passed to a styled component.</p>
-  </SectionLayout>
-)
+  \`\`\`react
+  const Button = styled.button.attrs({
+    className: 'small',
+  })\`
+    background: black;
+    color: white;
+    cursor: pointer;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid black;
+    border-radius: 3px;
+  \`;
+
+  render(
+    <div>
+      <Button>Styled Components</Button>
+      <Button>The new way to style components!</Button>
+    </div>
+  );
+  \`\`\`
+
+  Please do read about [the attrs method](/docs/api#attrs) to learn how arbitary props can be passed
+  down to a styled component without wrapping it.
+`
 
 export default CSSFrameworks
