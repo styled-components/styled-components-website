d6c63ed1c6b24302f83fbf2fc20926b8fb77396b
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-09-14 17:33:40 +0430

    docs: add info about extend and styled diff

--- sections/faqs/extend-and-styled-difference.js
+++ sections/faqs/extend-and-styled-difference.js
@@ -3,34 +3,23 @@ import md from 'components/md'
 const CSSFrameworks = () => md`
   ## What is the difference between \`Comp.extend\` and \`styled(Comp)\`?
 
-  Integrating an existing CSS framework with styled-components is really easy! You can use its existing class names alongside your components.
 
-  Consider you have an existing app with some CSS that have the classes: \`.small\` and \`.big\`.
-  Try to swap out the \`.small\` class with \`.big\` in the example below:
+  You have two choices to add additional styles to a component or a styled component,
+  one is using [\`Comp.extend\`](/docs/basics#extending-styles) and the other is wrapping your
+  component in the styled method like this: [\`styled(Comp)\`](/docs/basics#styling-any-components).
 
-  \`\`\`react
-  const Button = styled.button.attrs({
-    className: 'small',
-  })\`
-    background: black;
-    color: white;
-    cursor: pointer;
-    margin: 1em;
-    padding: 0.25em 1em;
-    border: 2px solid black;
-    border-radius: 3px;
-  \`;
+  The styled method works perfectly on all of your own or any third-party components
+  as well, but the extend method can only be used on a styled component only.
 
-  render(
-    <div>
-      <Button>Styled Components</Button>
-      <Button>The new way to style components!</Button>
-    </div>
-  );
-  \`\`\`
+  Styled method always generates a real stylesheet with classes. The classnames are
+  then passed to the React component (including third party components) via the className prop.
+  Calling \`extend\` creates a new stylesheet by extending the old one, and thus doesn't
+  generate two classes for a single component.
 
-  Please do read about [the attrs method](/docs/api#attrs) to learn how arbitary props can be passed
-  down to a styled component without wrapping it.
+  [\`Comp.extend\`](/docs/basics#extending-styles) and the other is wrapping your
+  component in the styled method like this: [\`styled(Comp)\`](/docs/basics#styling-any-components).
+
+  Please do read about [\`Styling any components\`](/docs/basics#styling-any-components). and [\`extend\`](/docs/basics#extending-styles).
 `
 
 export default CSSFrameworks
