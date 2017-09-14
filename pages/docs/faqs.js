55c8930a27b2f3f2066178bde3dfbfc00a4fff96
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-09-14 17:12:08 +0430

    docs: add extend and styled diff in the FAQ

--- pages/docs/faqs.js
+++ pages/docs/faqs.js
@@ -3,12 +3,14 @@ import DocsLayout from '../../components/DocsLayout'
 
 import Nesting from '../../sections/faqs/nesting'
 import ReverseSelectors from '../../sections/faqs/reverse-selectors'
+import ExtendAndStyled from '../../sections/faqs/extend-and-styled-difference'
 import CSSFrameworks from '../../sections/faqs/support-for-css-frameworks'
 
 const FAQs = () => (
   <DocsLayout title="FAQs" description="Commonly asked questions about styled-components">
     <Nesting />
     <ReverseSelectors />
+    <ExtendAndStyled />
     <CSSFrameworks />
   </DocsLayout>
 )
