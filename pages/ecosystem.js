03f0f692b43c3c61c4446b885f94f4e910fcf3e6
Sara Vieira <hey@iamsaravieira.com>
2017-10-05 11:38:30 +0100

    fix(ecosystem) - Add intro and add spacing for li

--- pages/ecosystem.js
+++ pages/ecosystem.js
@@ -3,20 +3,23 @@ import DocsLayout from '../components/DocsLayout'
 import { getReadme } from '../utils/githubApi'
 import md from '../components/md'
 import Loading from '../components/Loading'
+import Link from '../components/Link'
 
 
 
 const Ecosystem = ({ readme }) => (
     <DocsLayout title="Ecosystem" description="Ecosystem of styled-components">
+    <p>
+      This is an incomplete list of awesome things built with styled-components. If you have something to share, please add it to the <Link href="https://github.com/styled-components/awesome-styled-components" inline>awesome-styled-components</Link> repo on GitHub and it will automatically show up here!
+    </p>
       {typeof readme !== 'string' ? <Loading /> : md(
-          `${readme}
+          `
+          ${readme}
 
 ### Contribute
 
 If you know any projects build with styled components contributions and suggestions are always welcome !
 Please read the [contribution guidelines](https://github.com/styled-components/awesome-styled-components/blob/master/contributing.md) first and submit a PR.
-
-
           `)}
     </DocsLayout>
   )
