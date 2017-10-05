39ce3b6e7ec4e5179857932c6a257cbdbb0120c7
Jinksi <ericjinks@gmail.com>
2017-10-05 20:34:41 +1000

    Update <Code /> style

--- components/Code.js
+++ components/Code.js
@@ -1,11 +1,22 @@
 import styled from 'styled-components'
 import { monospace } from '../utils/fonts'
+import { paleGrey } from '../utils/colors'
 
 import '../utils/prismTemplateString'
 
 const Code = styled.span`
+  background: ${paleGrey};
+  border-radius: 3px;
   font-family: ${monospace};
-  font-weight: 500;
+  font-size: 85%;
+  font-weight: 300;
+  padding: 0.15em 0;
+
+  &:before,
+  &:after {
+    letter-spacing: -0.2em;
+    content: "\00a0";
+  }
 `
 
 export default Code
