2719c1b1e11b0ceafd2b3be1a44712ba286407d4
Jinksi <ericjinks@gmail.com>
2017-10-07 17:36:23 +1000

    Increase font-size, remove background & vertically align

--- components/Code.js
+++ components/Code.js
@@ -1,22 +1,15 @@
 import styled from 'styled-components'
 import { monospace } from '../utils/fonts'
-import { paleGrey } from '../utils/colors'
 
 import '../utils/prismTemplateString'
 
 const Code = styled.span`
-  background: ${paleGrey};
-  border-radius: 3px;
+  display: inline-block;
   font-family: ${monospace};
-  font-size: 85%;
+  font-size: 90%;
   font-weight: 300;
-  padding: 0.15em 0;
-
-  &::before,
-  &::after {
-    letter-spacing: -0.2em;
-    content: "\00a0";
-  }
+  padding: 0 0.05em 0.1em;
+  vertical-align: bottom;
 `
 
 export default Code
