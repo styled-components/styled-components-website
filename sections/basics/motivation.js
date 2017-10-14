d4e91e93b527a9da09092be5a3d0083ae9cda000
ink8bit <ink8bit@users.noreply.github.com>
2017-10-14 15:44:58 +0300

    Add media rule from utils

--- sections/basics/motivation.js
+++ sections/basics/motivation.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { phone } from '../../utils/media'
 
 import rem from '../../utils/rem'
 import md from 'components/md'
@@ -17,22 +18,23 @@ const Video = styled.div.attrs({
   box-shadow: ${rem(1)} ${rem(1)} ${rem(20)} rgba(20, 20, 20, 0.27);
   margin: ${rem(35)} 0;
   position: relative;
-  padding-top: calc(1 / (16 / 9) * 100%);
-  height: 0;
-  
-  & iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
+  width: 560px;
+  height: 315px;
+  padding-top: 0;
+
+  ${phone(css`
+    padding-top: calc(1 / (16 / 9) * 100%);
+    height: 0;
     width: 100%;
-    height: 100%;
-  }
-  
-  @media (min-width: 768px) {
-    width: 560px;
-    height: 315px;
-    padding-top: 0;
-  }
+
+    & iframe {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+  `)}
 `
 
 const Motivation = () => md`
