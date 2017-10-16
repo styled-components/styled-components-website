99951a3f435382681046a0664e2503d590c857e3
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-10-16 11:54:26 +0330

    feat: extract correct ToC from markdown

---
+++ utils/escape.js
@@ -0,0 +1,10 @@
+function escape(html, encode) {
+  return html
+    .replace(!encode ? /&(?!#?\w+;)/g : /&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+}
+
+export default escape
