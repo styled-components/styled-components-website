4e9d9345a61dbf2ab3181cbe6227482879fad601
Victor Quiroz <victorhqc@gmail.com>
2017-10-30 15:34:35 +0100

    Tests for `basics` page

---
+++ test/pages/docs/basics.spec.js
@@ -0,0 +1,25 @@
+'use babel'
+
+import React from 'react'
+import { mount } from 'enzyme'
+import 'jest-styled-components'
+import { I18nextProvider } from 'react-i18next'
+import { mockRouter } from '../../../utils/test-utils'
+import { Basics } from '../../../pages/docs/basics'
+import i18n from '../../helpers/i18n'
+
+mockRouter()
+
+const wrapper = (language, props) => mount(
+  <I18nextProvider i18n={i18n} initialLanguage={language}>
+    <Basics />
+  </I18nextProvider>
+)
+
+test('Basics renders correctly', () => {
+  expect(wrapper('en').find(Basics)).toMatchSnapshot()
+})
+
+test('Basics renders in spanish', () => {
+  expect(wrapper('es').find(Basics)).toMatchSnapshot()
+})
