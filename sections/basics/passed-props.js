aff677ab4cf2dc38227b7cf453f8d90dd00bb04c
Victor Quiroz <victorhqc@gmail.com>
2017-10-30 12:18:17 +0100

    Continue with docs/basics Translations
    Still not done! Plenty of translations to make in this page still.

--- sections/basics/passed-props.js
+++ sections/basics/passed-props.js
@@ -1,32 +1,14 @@
+import { I18n } from 'react-i18next'
 import md from 'components/md'
 
-const PassedProps = () => md`
-  ## Passed props
+import {
+  DOCS_TRANSLATION,
+} from '../../constants/i18n'
 
-  styled-components pass on all their props.
-
-  This example shows how all props of the Input component are passed on to the
-  DOM node that is mounted, as with React elements.
-
-  \`\`\`react
-  // Create an Input component that'll render an <input> tag with some styles
-  const Input = styled.input\`
-    padding: 0.5em;
-    margin: 0.5em;
-    color: palevioletred;
-    background: papayawhip;
-    border: none;
-    border-radius: 3px;
-  \`;
-
-  // Render a styled text input with a placeholder of "@mxstbr", and one with a value of "@geelen"
-  render(
-    <div>
-      <Input placeholder="@mxstbr" type="text" />
-      <Input value="@geelen" type="text" />
-    </div>
-  );
-  \`\`\`
-`
+const PassedProps = () => (
+  <I18n ns={DOCS_TRANSLATION}>
+    {translate => md(translate('passedProps'))}
+  </I18n>
+)
 
 export default PassedProps
