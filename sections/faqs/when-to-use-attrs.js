b6d323fbac9921679e03849b1e682c9c468d50da
Kadi Kraman <kadi.kraman@formidable.com>
2018-01-09 16:26:39 +0000

    Add a section to FAQs on when to use attrs

---
+++ sections/faqs/when-to-use-attrs.js
@@ -0,0 +1,24 @@
+import md from 'components/md'
+
+const WhenToUseAttrs = () => md`
+  ## When to use attrs?
+
+  You can pass in attributes to styled components using \`attrs\`, but
+  it is not always sensible to do so.
+
+  The rule of thumb is to use \`attrs\` when you want every instance of a styled
+  component to have that prop, and pass props directly when every instance needs a
+  different one:
+
+  \`\`\`js
+  const PasswordInput = styled.input.attrs({
+    // Every <PasswordInput /> should be type="password"
+    type: 'password'
+  })\`\`
+
+  // This specific one is hidden, so let's set aria-hidden
+  <PasswordInput aria-hidden="true" />
+  \`\`\`
+`
+
+export default WhenToUseAttrs
