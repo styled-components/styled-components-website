fa8695bcdd758eccc63a0af9759a49bf6e7c38d6
Sergey Petushkov <petushkov.sergey@gmail.com>
2018-01-10 19:09:21 +0100

    Fix errors

--- sections/faqs/library-authors.js
+++ sections/faqs/library-authors.js
@@ -3,9 +3,9 @@ import md from 'components/md'
 const LibraryAuthors = () => md`
   ## I am a library author. Should I bundle \`styled-components\` with my library?
 
-  If you are a library author we recommend that you should not bundle \`styled-components\` 
-  module with your library. Instead, mark it as external dependency during your library build, 
-  move it from \`dependencies\` to \`devDependencies\` and include it to your \`peerDependencies\` 
+  If you are a library author we recommend that you should not bundle \`styled-components\`
+  module with your library. Instead, mark it as external dependency during your library build,
+  move it from \`dependencies\` to \`devDependencies\` and include it in \`peerDependencies\`
   list in your \`package.json\` file:
 
   \`\`\`diff
