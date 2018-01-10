8ed10e001347533f3524757caa2a9bb3d277588e
Andrew Salcedo <asalcedo07@gmail.com>
2018-01-10 18:16:46 +0100

    fix prose in browser support faq

--- sections/faqs/browser-support.js
+++ sections/faqs/browser-support.js
@@ -1,11 +1,11 @@
 import md from "components/md"
 
 const BrowserSupport = () => md`
-  ## Which browsers does styled-components support?
+  ## Which browsers are supported?
 
-  Styled-components supports all the browsers that ReactDOM supports.
-  Please do read the [React docs section](https://reactjs.org/docs/react-dom.html#browser-support) to learn more.
-  However, the short version is that ReactDOM currently supports "IE9+", i.e. any browser and version that was released since - and including - IE9. 
-`
+  All [browsers that React supports](https://reactjs.org/docs/react-dom.html#browser-support) are supported.
+
+  All major browsers including Internet Explorer 9 and above are supported.
+`;
 
 export default BrowserSupport
