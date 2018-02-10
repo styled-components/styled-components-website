f7260bb2fbf806d821485980ee4006e32845d9a6
Phil Plückthun <phil@plckthn.me>
2018-02-10 00:24:58 +0000

    Upgrade tests to React 16 and update snapshots
    - Update Enzyme (React 16 adapter)
    - Update enzyme-to-json
    - Fix incorrect styled-components dependency
    - Fix package.json dependencies/devDependencies mashups
    - Update snapshots
    - Misc

--- .jest.config.js
+++ .jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/integration-test/'],
+  setupFiles: [
+    './.jest.setup.js'
+  ],
   snapshotSerializers: [
     'enzyme-to-json/serializer'
   ]
