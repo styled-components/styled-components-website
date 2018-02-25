b6b089031025c08c72c16163ac626febd398e2c2
Kevin McKernan <kevin@mckernan.in>
2018-02-24 17:39:13 -0700

    download from imgur to static folder

--- sections/tooling/syntax-highlighting.js
+++ sections/tooling/syntax-highlighting.js
@@ -7,7 +7,7 @@ const SyntaxHighlighting = () => md`
 
 This is what it looks like when properly highlighted:
 
-<img alt="Syntax highlighted styled component" src="https://imgur.com/k7h45c3.jpg" height="250px" />
+<img alt="Syntax highlighted styled component" src="/static/syntax-highlight-example.jpg" height="250px" />
 
 ### Atom
 
