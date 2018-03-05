c30c2af143da85c7a94b1ef27ac92908e3236357
Richard Hallows <jeddy3@users.noreply.github.com>
2018-03-05 18:08:22 +0000

    Port over stylelint-processor-styled-components doc changes (#241)
    * Use stylelint-config-recommended in examples
    
    * Use default syntax in stylelint examples

--- sections/tooling/stylelint.js
+++ sections/tooling/stylelint.js
@@ -12,7 +12,7 @@ You need:
 - \`stylelint\` (duh)
 - This processor, to extract styles from \`styled-components\`
 - The [\`stylelint-config-styled-components\`](https://github.com/styled-components/stylelint-config-styled-components) to disable stylelint rules that clash with \`styled-components\`
-- Your favorite \`stylelint\` config! (for example [\`stylelint-config-standard\`](https://github.com/stylelint/stylelint-config-standard))
+- Your favorite \`stylelint\` config! (for example [\`stylelint-config-recommended\`](https://github.com/stylelint/stylelint-config-recommended))
 
 > We recommend using Stylelint v9+ as this has added features that allow us to report correct line numbers on CSS syntax errors
 
@@ -21,7 +21,7 @@ You need:
   stylelint
   stylelint-processor-styled-components
   stylelint-config-styled-components
-  stylelint-config-standard)
+  stylelint-config-recommended)
 \`\`\`
 
 ### Setup
@@ -32,14 +32,11 @@ Add a \`.stylelintrc\` file to the root of your project:
 {
   "processors": ["stylelint-processor-styled-components"],
   "extends": [
-    "stylelint-config-standard",
+    "stylelint-config-recommended",
     "stylelint-config-styled-components"
-  ],
-  "syntax": "scss"
+  ]
 }
 \`\`\`
-
-> Setting the \`syntax\` to \`scss\` is needed for nesting and interpolation support!
 
 Then you need to run \`stylelint\`. Add a \`lint:css\` script to your \`package.json\` which runs \`stylelint\` with a glob to all of your styled components:
 
