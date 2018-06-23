ccf0dc12cf907c693f63cdf26d15d85c51003ea4
Jason Miazga <jmiazga@gmail.com>
2018-06-23 15:12:04 -0400

    added section about defining styled components outside of render method

--- sections/basics/coming-from-css.js
+++ sections/basics/coming-from-css.js
@@ -60,6 +60,33 @@ component \`Counter\` and the Styled Component \`StyledCounter\`
 don't clash names but remain easily identifiable in the React Developer 
 Tools and Web Inspector.
 
+### Define Styled Components outside of the render method
+
+It is important to define your styled components outside of render method, 
+otherwise the styled component will be recreated on every single render pass.
+Defining a styled component within the render method is bad for performance
+and should be avoided.
+
+Write your styled components the recommend way:
+
+\`\`\`jsx
+const StyledWrapper = styled.div\`/* ... */\`
+
+const Wrapper = ({message}) => {
+  return <StyledWrapper>{message}</StyledWrapper>
+}
+\`\`\`
+
+Instead of:
+
+\`\`\`jsx
+const Wrapper = ({message}) => {
+  const StyledWrapper = styled.div\`/* ... */\`
+
+  return <StyledWrapper>{message}</StyledWrapper>
+}
+\`\`\`
+
 **Recommended reading**: [Talia Marcassa](https://twitter.com/talialongname) 
 wrote a great review of real-world usage, featuring lots of solid practical insights 
 and comparisons with alternatives, in [Styled Components: To Use or Not to Use?](https://medium.com/building-crowdriff/styled-components-to-use-or-not-to-use-a6bb4a7ffc21)
