a293fb2f3b1877c0602de9cef96afa76a71a5270
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2018-09-21 19:36:51 +0530

    Move to fontawesome

--- components/Nav/MobileNavbar.js
+++ components/Nav/MobileNavbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
-
 import rem from '../../utils/rem'
 import { navbarHeight } from '../../utils/sizes'
 import { paleGrey } from '../../utils/colors'
