3dc9f54a0593c6fbe307aadef13c675037403801
Adam Snodgrass <overra@gmail.com>
2018-10-27 20:07:25 -0500

    clean up with prettier

--- components/ShowcaseLayout.js
+++ components/ShowcaseLayout.js
@@ -1,45 +1,33 @@
-import React, { Component } from 'react'
-import Head from './SeoHead'
-import Nav from './Nav'
+import React, { Component } from 'react';
+import Head from './SeoHead';
+import Nav from './Nav';
 
 class ShowcaseLayout extends Component {
   state = {
     isSideFolded: true,
-    isMobileNavFolded: true
-  }
+    isMobileNavFolded: true,
+  };
 
   static defaultProps = {
     title: '',
     description: '',
-  }
+  };
 
   render() {
-    const {
-      children,
-      title,
-      description,
-    } = this.props
+    const { children, title, description } = this.props;
 
     return (
       <div>
-
-        <Head
-          title={`styled-components${title ? `: ${title}` : ''}`}
-          description={description}>
-            <meta name="robots" content="noodp" />
+        <Head title={`styled-components${title ? `: ${title}` : ''}`} description={description}>
+          <meta name="robots" content="noodp" />
         </Head>
 
-        <Nav
-          showSideNav={false}
-        />
+        <Nav showSideNav={false} />
 
-        <div style={{paddingTop: 90}}>
-          {children}
-        </div>
+        <div style={{ paddingTop: 90 }}>{children}</div>
       </div>
-    )
+    );
   }
 }
 
-
-export default ShowcaseLayout
+export default ShowcaseLayout;
--- pages/showcase.js
+++ pages/showcase.js
@@ -1,100 +1,99 @@
-import React from 'react'
-import ShowcaseLayout from '../components/ShowcaseLayout'
-import { sortOrder, mapping } from '../showcase-manifest'
-import styled from 'styled-components'
-import Slider from '../components/Slider'
-import { generateShowcaseUrl } from '../components/Slider/ShowcaseLink'
-import Router from 'next/router'
+import React from 'react';
+import ShowcaseLayout from '../components/ShowcaseLayout';
+import { sortOrder, mapping } from '../showcase-manifest';
+import styled from 'styled-components';
+import Slider from '../components/Slider';
+import { generateShowcaseUrl } from '../components/Slider/ShowcaseLink';
+import { withRouter } from 'next/router';
 
 // Returns the right slide index based on the current position
 function normalizeSlideIndex(arr, index, fn) {
   // The logic doesn't care about the implementation, just the result
-  const result = fn(index)
+  const result = fn(index);
 
   // If the result is bigger than the length of the array we return the start of the array
-  if(result > (arr.length - 1)) {
-    return 0
+  if (result > arr.length - 1) {
+    return 0;
   }
 
   // If the result is lower than the start of the array we return the end of the array
-  if(result < 0) {
-    return arr.length -1
+  if (result < 0) {
+    return arr.length - 1;
   }
 
   // If the result is within the array parameters we return it
-  return result
+  return result;
 }
 
 // Since objects don't allow for a sort order we have to map an array to the object
 function mapIndexToRoute(index) {
-  const route = sortOrder[index]
-  return mapping[route]
+  const route = sortOrder[index];
+  return mapping[route];
 }
 
 function calculateSlides(sortOrder, route) {
-  let currentSlideIndex = sortOrder.indexOf(route)
-  if(currentSlideIndex === -1) {
-    currentSlideIndex = 0
+  let currentSlideIndex = sortOrder.indexOf(route);
+  if (currentSlideIndex === -1) {
+    currentSlideIndex = 0;
   }
-  const previousSlideIndex = normalizeSlideIndex(sortOrder, currentSlideIndex, (x) => x - 1)
-  const nextSlideIndex = normalizeSlideIndex(sortOrder, currentSlideIndex, (x) => x + 1)
+  const previousSlideIndex = normalizeSlideIndex(sortOrder, currentSlideIndex, x => x - 1);
+  const nextSlideIndex = normalizeSlideIndex(sortOrder, currentSlideIndex, x => x + 1);
   return {
     currentSlide: mapIndexToRoute(currentSlideIndex),
     previousSlide: mapIndexToRoute(previousSlideIndex),
-    nextSlide: mapIndexToRoute(nextSlideIndex)
-  }
+    nextSlide: mapIndexToRoute(nextSlideIndex),
+  };
 }
 
 class ArrowEvents extends React.Component {
-  handleKeyDown = (event) => {
-    const isLeft = event.keyCode === 37
-    const isRight = event.keyCode === 39
+  handleKeyDown = event => {
+    const isLeft = event.keyCode === 37;
+    const isRight = event.keyCode === 39;
 
-    const {router, previousSlide, nextSlide} = this.props
-    console.log(previousSlide, nextSlide)
-    if(isLeft) {
-      const {href, as} = generateShowcaseUrl(previousSlide)
-      router.replace(href, as)
-      return
+    const { router, previousSlide, nextSlide } = this.props;
+    if (isLeft) {
+      const { href, as } = generateShowcaseUrl(previousSlide);
+      router.replace(href, as);
+      return;
     }
 
-    if(isRight) {
-      const {href, as} = generateShowcaseUrl(nextSlide)
-      router.replace(href, as)
-      return
+    if (isRight) {
+      const { href, as } = generateShowcaseUrl(nextSlide);
+      router.replace(href, as);
+      return;
     }
-  }
+  };
 
   componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyDown)
+    document.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown)
+    document.removeEventListener('keydown', this.handleKeyDown);
   }
 
   render() {
-    return null
+    return null;
   }
 }
 
 const Screen = styled.div`
   display: flex;
   align-items: center;
-  min-height: ${props => props.offset ? `calc(100vh - ${props.offset}px)` : '100vh'};
-`
+  min-height: ${props => (props.offset ? `calc(100vh - ${props.offset}px)` : '100vh')};
+`;
 
-const Showcase = ({url}) => {
-  const { item } = url.query 
-  const {currentSlide, previousSlide, nextSlide} = calculateSlides(sortOrder, item)
+const Showcase = ({ router }) => {
+  const { item } = router.query;
+  const { currentSlide, previousSlide, nextSlide } = calculateSlides(sortOrder, item);
   return (
     <ShowcaseLayout title="Showcase" description="Screenshots of websites that use styled-components">
-      <ArrowEvents router={Router} previousSlide={previousSlide} nextSlide={nextSlide} />
+      <ArrowEvents router={router} previousSlide={previousSlide} nextSlide={nextSlide} />
       <Screen offset={90}>
         <Slider currentSlide={currentSlide} previousSlide={previousSlide} nextSlide={nextSlide} />
       </Screen>
     </ShowcaseLayout>
-  )
-}
+  );
+};
 
-export default Showcase
+export default withRouter(Showcase);
