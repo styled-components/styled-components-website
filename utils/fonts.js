f2b172c373cac81755bd49235a0cfd9f4e05f7b7
Phil Pluckthun <phil@kitten.sh>
2018-10-29 20:58:51 +0000

    Add note to footer

--- utils/fonts.js
+++ utils/fonts.js
@@ -1,4 +1,4 @@
 export const bodyFont =
   '-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
 export const headerFont = `"Avenir Next", ${bodyFont}`;
-export const monospace = '"Dank Mono", dm, monospace';
+export const monospace = 'dm, monospace';
