17ca0dbc75577f24d2eddd8401b70798cf285f05
Evan Jacobs <probablyup@gmail.com>
2019-06-10 23:13:00 -0500

    update jest

--- test/components/NavBar/SidebarMenus.spec.js
+++ test/components/NavBar/SidebarMenus.spec.js
@@ -1,19 +1,15 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import 'jest-styled-components'
-import {
-  DocsSidebarMenu,
-  SimpleSidebarMenu,
-} from '../../../components/Nav/SidebarMenus'
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { DocsSidebarMenu, SimpleSidebarMenu } from '../../../components/Nav/SidebarMenus';
 
 test('DocsSidebarMenu renders correctly', () => {
-  const tree = renderer.create(<DocsSidebarMenu />).toJSON()
+  const tree = renderer.create(<DocsSidebarMenu />).toJSON();
 
-  expect(tree).toMatchSnapshot()
-})
+  expect(tree).toMatchSnapshot();
+});
 
 test('SimpleSidebarMenu renders correctly', () => {
-  const tree = renderer.create(<SimpleSidebarMenu />).toJSON()
+  const tree = renderer.create(<SimpleSidebarMenu />).toJSON();
 
-  expect(tree).toMatchSnapshot()
-})
+  expect(tree).toMatchSnapshot();
+});
