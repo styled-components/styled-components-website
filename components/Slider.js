604ccb81cdc505d98c020de54ec3eaa7a08285ce
Ayoub <ayoub@aabass.net>
2019-10-11 00:16:30 +0200

    ✏️ slideshow now scales based on screen size

--- components/Slider.js
+++ components/Slider.js
@@ -7,6 +7,17 @@ import Link from './Link';
 import rem from '../utils/rem';
 import { headerFont } from '../utils/fonts';
 
+// TODO: replace these variables if needed
+const NavWidth = 192;
+const NavHeight = 144;
+const NavOffset = 32;
+const ScreenMultipliers = {
+  xs: 0.3,
+  sm: 0.5,
+  md: 0.75,
+  lg: 1,
+};
+
 const Section = styled.section`
   display: flex;
   min-height: 0;
@@ -19,11 +30,29 @@ const Section = styled.section`
 `;
 
 const Nav = styled.div`
-  width: 200px;
-  min-width: 200px;
-  height: 150px;
+  width: ${NavWidth * ScreenMultipliers.xs}px;
+  min-width: ${NavWidth * ScreenMultipliers.xs}px;
+  height: ${NavHeight * ScreenMultipliers.xs}px;
   overflow: hidden;
   transition: all 0.2s ease;
+
+  @media screen and (min-width: 420px) {
+    width: ${NavWidth * ScreenMultipliers.sm}px;
+    min-width: ${NavWidth * ScreenMultipliers.sm}px;
+    height: ${NavHeight * ScreenMultipliers.sm}px;
+  }
+
+  @media screen and (min-width: 800px) {
+    width: ${NavWidth * ScreenMultipliers.md}px;
+    min-width: ${NavWidth * ScreenMultipliers.md}px;
+    height: ${NavHeight * ScreenMultipliers.md}px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: ${NavWidth * ScreenMultipliers.lg}px;
+    min-width: ${NavWidth * ScreenMultipliers.lg}px;
+    height: ${NavHeight * ScreenMultipliers.lg}px;
+  }
 
   &:hover {
     transition: all 0.2s ease-in-out;
@@ -32,30 +61,102 @@ const Nav = styled.div`
   ${props =>
     props.prev
       ? `
-    margin-left: -40px;          
-    border-top-right-radius: 5px;
-    border-bottom-right-radius: 5px;
+    margin-left: -${NavOffset * ScreenMultipliers.xs}px;        
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
     &:hover {
-      transform: translateX(40px);
+      transform: translateX(${NavOffset * ScreenMultipliers.xs}px);
+    }
+
+    @media screen and (min-width: 420px) {
+      margin-left: -${NavOffset * ScreenMultipliers.sm}px;
+
+      &:hover {
+        transform: translateX(${NavOffset * ScreenMultipliers.sm}px);
+      }
+    }
+    
+    @media screen and (min-width: 800px) {
+      margin-left: -${NavOffset * ScreenMultipliers.md}px;
+
+      &:hover {
+        transform: translateX(${NavOffset * ScreenMultipliers.md}px);
+      }
+    }
+    
+    @media screen and (min-width: 1280px) {
+      margin-left: -${NavOffset * ScreenMultipliers.lg}px;
+
+      &:hover {
+        transform: translateX(${NavOffset * ScreenMultipliers.lg}px);
+      }
     }
   `
       : `
-    margin-right: -40px;          
-    border-top-left-radius: 5px;
-    border-bottom-left-radius: 5px;
+    margin-right: -${NavOffset * ScreenMultipliers.xs}px;          
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
     &:hover {
-      transform: translateX(-40px);
+      transform: translateX(-${NavOffset * ScreenMultipliers.xs}px);
+    }
+
+    @media screen and (min-width: 420px) {
+      margin-right: -${NavOffset * ScreenMultipliers.sm}px;
+
+      &:hover {
+        transform: translateX(-${NavOffset * ScreenMultipliers.sm}px);
+      }
+    }
+    
+    @media screen and (min-width: 800px) {
+      margin-right: -${NavOffset * ScreenMultipliers.md}px;
+
+      &:hover {
+        transform: translateX(-${NavOffset * ScreenMultipliers.md}px);
+      }
+    }
+    
+    @media screen and (min-width: 1280px) {
+      margin-right: -${NavOffset * ScreenMultipliers.lg}px;
+
+      &:hover {
+        transform: translateX(-${NavOffset * ScreenMultipliers.lg}px);
+      }
     }
   `};
 `;
 
+const Body = styled.div`
+  max-width: 1280px;
+  width: calc(100% - ${NavWidth * ScreenMultipliers.xs * 2}px);
+
+  @media screen and (min-width: 420px) {
+    width: calc(100% - ${NavWidth * ScreenMultipliers.sm * 2}px);
+  }
+
+  @media screen and (min-width: 800px) {
+    width: calc(100% - ${NavWidth * ScreenMultipliers.md * 2}px);
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: calc(100% - ${NavWidth * ScreenMultipliers.lg * 2}px);
+  }
+`;
+
 const ImageSlider = styled.div`
   flex: 1;
-  margin: 0 60px;
+  margin: 0 16px;
   box-shadow: 0 30px 40px rgba(0, 0, 0, 0.12);
-  max-width: 1280px;
+  max-width: 100%;
   border-radius: 4px;
   overflow: hidden;
+
+  @media screen and (min-width: 800px) {
+    margin: 0 32px;
+  }
+  @media screen and (min-width: 1280px) {
+    margin: 0 48px;
+  }
 `;
 
 const Caption = styled.div`
@@ -78,7 +179,7 @@ const Slider = ({ previousSlide, currentSlide, nextSlide }) => (
     <Nav prev>
       <Navigation previous item={previousSlide} />
     </Nav>
-    <div>
+    <Body>
       <Caption>
         <Title>{currentSlide.title}</Title>
         <Link inline href={currentSlide.link}>
@@ -103,7 +204,7 @@ const Slider = ({ previousSlide, currentSlide, nextSlide }) => (
           }}
         />
       </ImageSlider>
-    </div>
+    </Body>
     <Nav>
       <Navigation item={nextSlide} />
     </Nav>
