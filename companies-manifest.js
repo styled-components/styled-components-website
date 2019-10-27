11590fbdafc2b7d9ffe68795fcb0b29770f1c318
Ayoub <ayoub@aabass.net>
2019-10-27 11:30:41 +0100

    ✏️companies manifest tweak

--- companies-manifest.js
+++ companies-manifest.js
@@ -50,296 +50,377 @@ import {
 
 const companies = [
   {
-    bottom: '-0.1rem',
-    height: '2rem',
     key: 'https://www.autodesk.com',
     similarWebGlobalRank: 1425,
     logo: AutodeskLogo,
+    style: {
+      bottom: '-0.1rem',
+      height: '2rem',
+    },
   },
   {
-    bottom: '0.3rem',
-    height: '2.5rem',
     key: 'https://www.airbnb.com',
     similarWebGlobalRank: 20,
     logo: AirBnBLogo,
+    style: {
+      bottom: '0.3rem',
+      height: '2.5rem',
+    },
   },
   {
-    bottom: '0.16rem',
-    height: '2.25rem',
     key: 'https://www.artsy.net',
     similarWebGlobalRank: 17576,
     logo: ArtsyLogo,
+    style: {
+      bottom: '0.16rem',
+      height: '2.25rem',
+    },
   },
   {
-    height: '1.75rem',
     key: 'https://www.atlassian.com/',
     similarWebGlobalRank: 2505,
     logo: AtlassianLogo,
+    style: {
+      height: '1.75rem',
+    },
   },
   {
     key: 'https://auth0.com',
     similarWebGlobalRank: 5628,
     logo: Auth0Logo,
+    style: {},
   },
   {
     key: 'https://www.bbc.com/',
     similarWebGlobalRank: 114,
     logo: BBCLogo,
+    style: {},
   },
   {
     key: 'https://www.blablacar.fr',
     similarWebGlobalRank: 3803,
     logo: BlaBlaCarLogo,
+    style: {},
   },
   {
     key: 'https://brave.com/',
     similarWebGlobalRank: 13395,
     logo: BraveLogo,
+    style: {},
   },
   {
-    bottom: '-0.2rem',
-    height: '1.75rem',
     key: 'https://www.bloomberg.com',
     similarWebGlobalRank: 507,
     logo: BloombergLogo,
+    style: {
+      bottom: '-0.2rem',
+      height: '1.75rem',
+    },
   },
   {
-    bottom: '0.75rem',
-    height: '3.3rem',
     key: 'https://www.blue-tomato.com',
     similarWebGlobalRank: 21322,
     logo: BlueTomatoLogo,
+    style: {
+      bottom: '0.75rem',
+      height: '3.3rem',
+    },
   },
   {
-    bottom: '-0.2rem',
     key: 'http://casper.com/',
     similarWebGlobalRank: 33785,
     logo: CasperLogo,
+    style: {
+      bottom: '-0.2rem',
+    },
   },
   {
-    height: '2rem',
     key: 'https://theculturetrip.com',
     similarWebGlobalRank: 4945,
     logo: CultureTripLogo,
+    style: {
+      height: '2rem',
+    },
   },
   {
-    bottom: '0.3rem',
-    height: '2rem',
-    src: '/static/coinbase-logo.svg',
     key: 'https://www.coinbase.com',
     similarWebGlobalRank: 2001,
     logo: CoinbaseLogo,
+    style: {
+      bottom: '0.3rem',
+      height: '2rem',
+    },
   },
   {
-    bottom: '-0.2rem',
     key: 'https://www.doordash.com/',
     similarWebGlobalRank: 2831,
     logo: DoorDashLogo,
+    style: {
+      bottom: '-0.2rem',
+    },
   },
   {
-    bottom: '0.1rem',
-    height: '2.5rem',
     key: 'https://eurovision.tv/',
     similarWebGlobalRank: 38132,
     logo: EuroVisionLogo,
+    style: {
+      bottom: '0.1rem',
+      height: '2.5rem',
+    },
   },
   {
-    bottom: '0.1rem',
-    height: '2rem',
     key: 'https://eddiebauer.com',
     similarWebGlobalRank: 11625,
     logo: EddieBauerLogo,
+    style: {
+      bottom: '0.1rem',
+      height: '2rem',
+    },
   },
   {
     key: 'https://github.com/',
     similarWebGlobalRank: 76,
     logo: GitHubLogo,
+    style: {},
   },
   {
-    src: '/static/huffpost-logo.svg',
     key: 'https://www.huffpost.com',
     similarWebGlobalRank: 950,
     logo: HuffpostLogo,
+    style: {},
   },
   {
-    bottom: '-0.15rem',
-    height: '1.5rem',
     key: 'https://www.ideo.com',
     similarWebGlobalRank: 117384,
     logo: IdeoLogo,
+    style: {
+      bottom: '-0.15rem',
+      height: '1.5rem',
+    },
   },
   {
-    bottom: '0.1rem',
-    height: '2.25rem',
     key: 'https://www.invisionapp.com',
     similarWebGlobalRank: 2368,
     logo: InVisionLogo,
+    style: {
+      bottom: '0.1rem',
+      height: '2.25rem',
+    },
   },
   {
-    bottom: '0.3rem',
-    height: '2.75rem',
     key: 'https://www.kiwi.com',
     similarWebGlobalRank: 4612,
     logo: KiwiComLogo,
+    style: {
+      bottom: '0.3rem',
+      height: '2.75rem',
+    },
   },
   {
     key: 'https://www.lego.com',
     similarWebGlobalRank: 2008,
     logo: LegoLogo,
+    style: {},
   },
   {
-    bottom: '1rem',
-    height: '4rem',
     key: 'https://www.magicleap.com/',
     similarWebGlobalRank: 119765,
     logo: MagicLeapLogo,
+    style: {
+      bottom: '1rem',
+      height: '4rem',
+    },
   },
   {
-    bottom: '-0.15rem',
-    height: '1.3rem',
     key: 'https://www.moleskine.com',
     similarWebGlobalRank: 70404,
     logo: MoleskineLogo,
+    style: {
+      bottom: '-0.15rem',
+      height: '1.3rem',
+    },
   },
   {
     key: 'https://www.patreon.com/',
     similarWebGlobalRank: 533,
     logo: PatreonLogo,
+    style: {},
   },
   {
-    bottom: '0.15rem',
     key: 'https://www.reddit.com/',
     similarWebGlobalRank: 20,
     logo: RedditLogo,
+    style: {
+      bottom: '0.15rem',
+    },
   },
   {
     key: 'https://seatgeek.com',
     similarWebGlobalRank: 7292,
     logo: SeatGeekLogo,
+    style: {},
   },
   {
     key: 'https://www.sesamegifts.com/',
     similarWebGlobalRank: 500301,
     logo: SesameLogo,
+    style: {},
   },
   {
-    bottom: '0.4rem',
-    height: '3rem',
     key: 'https://www.shpock.com',
     similarWebGlobalRank: 11112,
     logo: ShpockLogo,
+    style: {
+      bottom: '0.4rem',
+      height: '3rem',
+    },
   },
   {
-    bottom: '0.1rem',
-    height: '2rem',
     key: 'https://smart.com',
     similarWebGlobalRank: 70739,
     logo: SmartLogo,
+    style: {
+      bottom: '0.1rem',
+      height: '2rem',
+    },
   },
   {
     key: 'https://www.target.com',
     similarWebGlobalRank: 300,
     logo: TargetLogo,
+    style: {},
   },
   {
-    height: '1.7rem',
     key: 'https://www.ticketmaster.com/',
     similarWebGlobalRank: 1079,
     logo: TicketmasterLogo,
+    style: {
+      height: '1.7rem',
+    },
   },
   {
-    bottom: '0.2rem',
     key: 'https://tinder.com/',
     similarWebGlobalRank: 561,
     logo: TinderLogo,
+    style: {
+      bottom: '0.2rem',
+    },
   },
   {
-    bottom: '0.15rem',
-    height: '2.35rem',
     key: 'https://www.typeform.com',
     similarWebGlobalRank: 3279,
     logo: TypeformLogo,
+    style: {
+      bottom: '0.15rem',
+      height: '2.35rem',
+    },
   },
   {
-    height: '2.3rem',
     key: 'https://www.underarmour.com',
     similarWebGlobalRank: 6221,
     logo: UnderArmourLogo,
+    style: {
+      height: '2.3rem',
+    },
   },
   {
-    bottom: '0.3rem',
-    height: '2.1rem',
     key: 'https://www.vimeo.com',
     similarWebGlobalRank: 239,
     logo: VimeoLogo,
+    style: {
+      bottom: '0.3rem',
+      height: '2.1rem',
+    },
   },
   {
-    height: '1.8rem',
     key: 'https://www.vogue.com',
     similarWebGlobalRank: 3075,
     logo: VogueLogo,
+    style: {
+      height: '1.8rem',
+    },
   },
   {
-    bottom: '0.7rem',
-    height: '2.5rem',
     key: 'https://www.xing.com',
     similarWebGlobalRank: 2203,
     logo: XingLogo,
+    style: {
+      bottom: '0.7rem',
+      height: '2.5rem',
+    },
   },
   {
-    height: '2.0rem',
     key: 'https://jane.com',
     similarWebGlobalRank: 21426,
     logo: JaneLogo,
+    style: {
+      height: '2.0rem',
+    },
   },
   {
-    height: '2.3rem',
     key: 'https://google.com',
     similarWebGlobalRank: 1,
     logo: GoogleLogo,
+    style: {
+      height: '2.3rem',
+    },
   },
   {
-    height: '1.5rem',
     key: 'https://genius.com',
     similarWebGlobalRank: 366,
     logo: GeniusLogo,
+    style: {
+      height: '1.5rem',
+    },
   },
   {
-    height: '2.3rem',
     key: 'https://www.eurostar.com',
     similarWebGlobalRank: 11380,
     logo: EurostarLogo,
+    style: {
+      height: '2.3rem',
+    },
   },
   {
-    bottom: '-0.2rem',
     key: 'http://change.org/',
     similarWebGlobalRank: 551,
     logo: ChangeOrgLogo,
+    style: {
+      bottom: '-0.2rem',
+    },
   },
   {
-    bottom: '0.1rem',
     key: 'https://www.trulia.com/',
     similarWebGlobalRank: 257,
     logo: TruliaLogo,
+    style: {
+      bottom: '0.1rem',
+    },
   },
   {
-    height: '1.3rem',
-    bottom: '-0.2rem',
     key: 'https://birchbox.com/',
     similarWebGlobalRank: 38199,
     logo: BirchboxLogo,
+    style: {
+      height: '1.3rem',
+      bottom: '-0.2rem',
+    },
   },
   {
-    height: '1.3rem',
     key: 'https://blockchain.com/',
     similarWebGlobalRank: 4797,
     logo: BlockchainLogo,
+    style: {
+      height: '1.3rem',
+    },
   },
   {
-    height: '2rem',
     key: 'https://www.volkswagen.de/de.html',
     similarWebGlobalRank: 75049,
     logo: VolkswagenLogo,
+    style: {
+      height: '2rem',
+    },
   },
 ];
 
