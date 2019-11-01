b1598ed60e43e8e709517f5867310bd9e4447496
Ayoub <ayoub@aabass.net>
2019-11-01 01:15:12 +0100

    ✏️ mobile and tablet layouts

--- components/SmallShowcase/index.js
+++ components/SmallShowcase/index.js
@@ -4,19 +4,78 @@ import styled from 'styled-components';
 const scaleFactor = [1.8, 1.4, 1];
 
 const Wrapper = styled.div`
-  width: 120%;
-  max-width: 1280px;
-  display: flex;
-  /* margin-top: 64px; */
-  transform: translateY(50%);
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 24px;
+  grid-row-gap: 32px;
+  padding: 24px;
+  transform: translateY(20%);
+  margin-top: -96px;
+
+  @media (min-width: 640px) {
+    grid-template-columns: repeat(3, 1fr);
+    transform: translateY(28%);
+    margin-top: -96px;
+    padding: 32px;
+  }
+
+  @media (min-width: 800px) {
+    width: 120%;
+    padding: 0;
+    max-width: 1280px;
+    margin-top: 0px;
+    display: flex;
+    transform: translateY(50%);
+  }
 `;
 
-const Website = styled.div`
+const Label = styled.label`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  max-width: 100%;
+  transform: translate(-50%, 50%);
+  background-color: white;
+  color: #333;
+  font-family: Avenir Next;
+  font-weight: bold;
+  font-size: 0.8rem;
+  padding: 2px 12px;
+  border-radius: 6px;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.08), 0 5px 12px rgba(0, 0, 0, 0.1);
+  opacity: 0;
+  transition: all 0.2s ease-out;
+
+  @media (min-width: 800px) {
+    font-size: 0.65rem;
+  }
+`;
+
+const Website = styled.a`
+  display: block;
   position: relative;
   width: 100%;
-  padding: 16px;
-  transform: scale(${props => scaleFactor[Math.abs(props.position - 2)]});
-  z-index: ${props => 2 - Math.abs(props.position - 2)};
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.1);
+
+    ${Label} {
+      opacity: 1;
+    }
+  }
+
+  @media (min-width: 800px) {
+    padding: 12px;
+    z-index: ${props => 2 - Math.abs(props.position - 2)};
+    display: ${props => (props.position > 4 ? 'none' : 'block')};
+    transform: scale(${props => scaleFactor[Math.abs(props.position - 2)]});
+
+    &:hover {
+      transform: scale(${props => scaleFactor[Math.abs(props.position - 2)] + 0.2});
+    }
+  }
 `;
 
 const RatioBox = styled.div`
@@ -42,11 +101,12 @@ const SmallShowcase = ({ projects }) => {
   return (
     <Wrapper>
       {Object.values(projects)
-        .slice(0, 5)
+        .slice(0, 6)
         .map((project, index) => (
-          <Website key={project.title} position={index}>
+          <Website key={project.title} position={index} href={project.link} target="_blank">
             <RatioBox>
               <Screenshot bg={project.src} />
+              <Label>{project.title}</Label>
             </RatioBox>
           </Website>
         ))}
