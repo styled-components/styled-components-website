40757dc43a0948994425796575226704955c019d
Ayoub <ayoub@aabass.net>
2019-11-05 17:15:54 +0100

    ✏️ homepage showcase links to main showcase

--- pages/showcase.js
+++ pages/showcase.js
@@ -85,6 +85,8 @@ const Screen = styled.div`
 `;
 
 const Showcase = ({ router }) => {
+  console.log('HELLO', router.query);
+
   const { item } = router.query;
   const { currentSlide, previousSlide, nextSlide } = calculateSlides(Object.keys(sortedProjects), item);
   return (
