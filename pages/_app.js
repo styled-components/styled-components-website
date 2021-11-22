2e341085066ad6e37da93f3af0a352ca8d5ff3f6
Evan Jacobs <ejacobs@aurorasolar.com>
2021-11-21 22:03:32 -0500

    remove tracking code

--- pages/_app.js
+++ pages/_app.js
@@ -18,12 +18,6 @@ export default class MyApp extends App {
 
           <meta name="theme-color" content="#da936a" />
           <meta name="author" content="styled-components" />
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WDWNSLK');`,
-            }}
-          />
         </Head>
         <MDXProvider components={components}>
           <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -167,11 +167,6 @@ export default class MyDocument extends Document {
         </Head>
 
         <body>
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDWNSLK" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          />
           <div className="root">
             <Main />
           </div>
