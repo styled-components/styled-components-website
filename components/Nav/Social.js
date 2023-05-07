d1f74c21dfc5108f0e5cbc1ed91640e98c8ecf59
Mishel Ghukasyan <55681212+Mish2j@users.noreply.github.com>
2023-05-06 18:45:01 -0700

    Update Social.js... added aria-label directly on SocialLink

--- components/Nav/Social.js
+++ components/Nav/Social.js
@@ -13,7 +13,6 @@ const Wrapper = styled.nav`
 
 const SocialLink = styled(Link).attrs(props => ({
   unstyled: true,
-  'aria-label': props.ariaLabel,
 }))`
   display: flex;
   margin-right: ${rem(20)};
@@ -76,16 +75,16 @@ const Spectrum = () => (
 
 const Social = props => (
   <Wrapper {...props}>
-    <SocialLink href="https://spectrum.chat/styled-components/" ariaLabel="Spectrum Chat">
+    <SocialLink href="https://spectrum.chat/styled-components/" aria-label="Spectrum Chat">
       <Spectrum />
     </SocialLink>
     {/* <SocialLink href="https://twitter.com/someone">
       <Twitter />
     </SocialLink> */}
-    <SocialLink href="https://github.com/styled-components" ariaLabel="GitHub">
+    <SocialLink href="https://github.com/styled-components" aria-label="GitHub">
       <StyledIcon as={Github} height="18" />
     </SocialLink>
-    <SocialLink href="https://medium.com/styled-components" ariaLabel="Medium.com">
+    <SocialLink href="https://medium.com/styled-components" aria-label="Medium.com">
       <StyledIcon as={MediumM} height="18" />
     </SocialLink>
   </Wrapper>
