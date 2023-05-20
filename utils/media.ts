c63915a7461afc0500a904b2d427ddb559fbca54
Masha <nasonkina@yandex.ru>
2023-05-20 12:13:53 +0300

    fix mobile & phone typings (media.ts)

--- utils/media.ts
+++ utils/media.ts
@@ -1,13 +1,14 @@
-import { css } from 'styled-components';
-import { RuleSet } from 'styled-components/dist/types';
+type CSSUtilType = ReturnType<typeof css>;
 
-export const mobile = (inner: ReturnType<typeof css>): RuleSet<object> => css`
+import { css } from 'styled-components';
+
+export const mobile = (inner: CSSUtilType): CSSUtilType => css`
   @media (max-width: ${1000 / 16}em) {
     ${inner};
   }
 `;
 
-export const phone = (inner: RuleSet<object>): RuleSet<object> => css`
+export const phone = (inner: CSSUtilType): CSSUtilType => css`
   @media (max-width: ${650 / 16}em) {
     ${inner};
   }
