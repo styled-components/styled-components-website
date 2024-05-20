2a59a8e05cdd2cb7fe0142faaeae051a2d6879ee
Angad Sethi <angad@easyagile.com>
2024-05-20 12:17:16 +1000

    Add in Rss feed icon and functionality + add a visually hidden component for screen readers

---
+++ components/VisuallyHidden.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const VisuallyHidden = styled.div`
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+`;
