09092e47ffc5e3f62f36c9646270178c0d58c36f
Evan Jacobs <ej@quantizor.dev>
2026-05-12 19:33:49 -0400

    feat(blog): per-post og:image, authorTwitter frontmatter, X logo in byline

--- components/BlogMeta.tsx
+++ components/BlogMeta.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import styled from 'styled-components';
-import { Github, Twitter } from '@styled-icons/fa-brands';
+import { Github } from '@styled-icons/fa-brands';
 import { ChevronDown, Globe } from '@styled-icons/boxicons-regular';
 import { theme, font } from '../utils/theme';
 import rem from '../utils/rem';
@@ -51,7 +51,7 @@ export function BlogByline({
               rel="noopener"
               aria-label={`${author} on X`}
             >
-              <Twitter />
+              <XLogo />
             </SocialIcon>
           )}
         </SocialLinks>
@@ -121,6 +121,14 @@ const SocialIcon = styled.a`
     height: ${rem(18)};
   }
 `;
+
+function XLogo() {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden>
+      <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+    </svg>
+  );
+}
 
 const CrossPostNote = styled.span`
   font-size: ${theme.text.sm};
